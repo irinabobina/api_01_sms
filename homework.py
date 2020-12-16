adf95011260deceb59ddbe656e8c9b5e4fc12f75
Ирина Бобина <bobin.ir495@gmail.com>
2020-12-16 18:04:25 +0300

    yfj

--- homework.py
+++ homework.py
@@ -20,7 +20,7 @@ def get_status(user_id):
     return status.json()['response'][0].get('online')  # Верните статус пользователя в ВК
 
 
-def send_sms(sms_text):
+def sms_sender(sms_text):
     account_sid = "ACb3f3d0a8a0cfad3c6f59b41cec333be6"
     auth_token = "df0a25b80324ee75bf19663b5a15eb44"
     client = Client(account_sid, auth_token)
@@ -38,6 +38,6 @@ if __name__ == '__main__':
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
-            send_sms(f'{vk_id} сейчас онлайн!')
+            sms_sender(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
