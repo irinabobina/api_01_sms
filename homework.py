182faaca0b7f649b5968049b008b5e9699dd5d23
Ирина Бобина <bobin.ir495@gmail.com>
2020-12-16 18:14:23 +0300

    fhtjf

--- homework.py
+++ homework.py
@@ -17,8 +17,8 @@ def get_status(user_id):
         'fields': 'online',
     }
     response = requests.post('https://api.vk.com/method/users.get', params=params).json()['response']
-    status = response[0].get('online') 
-    return status  # Верните статус пользователя в ВК
+    status_online = response[0].get('online') 
+    return status_online  # Верните статус пользователя в ВК
 
 
 def sms_sender(sms_text):
