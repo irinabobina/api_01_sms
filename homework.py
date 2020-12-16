e598a43c1aba455a32eea357c56447ca94ae2756
Ирина Бобина <bobin.ir495@gmail.com>
2020-12-16 18:02:49 +0300

    fgn

--- homework.py
+++ homework.py
@@ -2,7 +2,7 @@ import os
 import time
 
 import requests
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 from twilio.rest import Client
 
 load_dotenv()
@@ -20,8 +20,11 @@ def get_status(user_id):
     return status.json()['response'][0].get('online')  # Верните статус пользователя в ВК
 
 
-def send_sms(sms_text, client):
-    #client = Client(account_sid, auth_token)
+def send_sms(sms_text):
+    account_sid = "ACb3f3d0a8a0cfad3c6f59b41cec333be6"
+    auth_token = "df0a25b80324ee75bf19663b5a15eb44"
+    client = Client(account_sid, auth_token)
+
     message = client.messages.create(
                               body= sms_text,
                               from_=os.getenv('NUMBER_FROM'),
@@ -31,14 +34,10 @@ def send_sms(sms_text, client):
 
 
 if __name__ == '__main__':
-    
-    account_sid = "ACb3f3d0a8a0cfad3c6f59b41cec333be6"
-    auth_token = "df0a25b80324ee75bf19663b5a15eb44"
-    client = Client(account_sid, auth_token)
     # тут происходит инициализация Client
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
-            send_sms(f'{vk_id} сейчас онлайн!', client)
+            send_sms(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
