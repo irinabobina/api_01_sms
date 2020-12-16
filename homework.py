bb83eb3184cdba445deaff4b5e8875f11fafd826
Ирина Бобина <bobin.ir495@gmail.com>
2020-12-16 23:12:23 +0300

    commit

--- homework.py
+++ homework.py
@@ -10,15 +10,17 @@ load_dotenv()
 
 
 def get_status(user_id):
-    params = {
-        'user_ids': user_id,
-        'v': 5.92,
-        'access_token': os.getenv('ACCESS_TOKEN'),
-        'fields': 'online',
-    }
-    response = requests.post('https://api.vk.com/method/users.get', params=params).json()['response']
-    status_online = response[0].get('online') 
-    return status_online  # Верните статус пользователя в ВК
+    params = {"user_ids": user_id,
+              "v": 5.92,
+              "fields": "online",
+              "access_token": os.getenv('ACCESS_TOKEN')
+              }
+
+    response = requests.post(url='https://api.vk.com/method/users.get',
+                             params=params).json()["response"]
+    status = response[0].get("online")
+    return status
+
 
 
 def sms_sender(sms_text):
@@ -32,15 +34,17 @@ def sms_sender(sms_text):
                               from_=os.getenv('NUMBER_FROM'),
                               to=os.getenv('NUMBER_TO')
                           )
-    return message.sid  # Верните sid отправленного сообщения из Twilio
-
+    return message.sid  
 
 if __name__ == '__main__':
-    # тут происходит инициализация Client
-
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
             sms_sender(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
+
+#небольшой комментарий: в заготовке функция send_sms получала два аргумента, но тесты потребовали
+#её переименовать, а также сделать, чтобы она получала один аргумент,
+#поэтому клиент не там, где было отмечено,
+#иначе не проходят тесты
